88bb4aa3b1b8b022ef4a57cd5c39f4e4dd77d880
ammaral1 <ammar.alqaraghuli@gmail.com>
2023-04-05 10:58:49 -0400

    light feature added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         SolarSystem s=new SolarSystem();
         s.color="red";
+
+        Light light=new Light();
+        light.color="red";
+
     }
 }
