dea63923399df82e4ffec397513fc41160781a5a
ammaral1 <ammar.alqaraghuli@gmail.com>
2023-04-05 10:51:52 -0400

    Sun feature added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         SolarSystem s=new SolarSystem();
         s.color="red";
+
+        Sun sun=new Sun();
+        sun.color="red";
+        
     }
 }
